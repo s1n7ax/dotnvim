8d97d5951bb1049227c32c49883e458e18543afd
s1n7ax <srineshnisala@gmail.com>
2023-06-20 13:27:14 +0530

    refactor: change <c-?> insert mode keymaps to <m-?>

--- lua/nvim/general/keybinds/insert.lua
+++ lua/nvim/general/keybinds/insert.lua
@@ -1,11 +1,11 @@
 local wk = require('which-key')
 
 wk.register({
-	['<c-a>'] = { '<esc>I', '(Insert) Jump to line start' },
-	['<c-m>'] = { '<esc>o','Insert new line below' },
-	['<c-h>'] = { '<esc>O', 'Insert new line above' },
-	['<c-k>'] = { '<esc>ddi', 'Delete current line' },
-	['<c-y>'] = {
+	['<m-a>'] = { '<esc>I', '(Insert) Jump to line start' },
+	['<m-h>'] = { '<esc>o', 'Insert new line below' },
+	['<m-s-h>'] = { '<esc>O', 'Insert new line above' },
+	['<m-k>'] = { '<esc>ddi', 'Delete current line' },
+	['<m-y>'] = {
 		function()
 			local buffer = vim.api.nvim_get_current_buf()
 			local cursor = vim.api.nvim_win_get_cursor(0)
