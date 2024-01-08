b1d252c3e6a0094df9e9d0e96bdfed41fdcf4fc3
s1n7ax <srineshnisala@gmail.com>
2024-01-09 00:04:44 +0530

    fix: nvim-java is not working

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,16 +1,8 @@
-local file = require('utils.file')
+local f = require('utils.file').is_pkg_dir
 
 return {
-	'nvim-java/nvim-java',
-	dir = file.is_pkg_dir('~/Workspace/nvim-java'),
+	'williamboman/mason-lspconfig.nvim',
 	dependencies = {
-		'nvim-java/lua-async-await',
-		'nvim-java/nvim-java-core',
-		'nvim-java/nvim-java-test',
-		'nvim-java/nvim-java-dap',
-		'MunifTanjim/nui.nvim',
-		'neovim/nvim-lspconfig',
-		'mfussenegger/nvim-dap',
 		{
 			'williamboman/mason.nvim',
 			opts = {
@@ -20,57 +12,39 @@ return {
 				},
 			},
 		},
+
+		{
+			'nvim-java/nvim-java',
+			dir = f('~/Workspace/nvim-java'),
+			opts = {},
+			dependencies = {
+				{
+					'nvim-java/lua-async-await',
+					dir = f('~/Workspace/lua-async-await'),
+				},
+				{
+					'nvim-java/nvim-java-core',
+					dir = f('~/Workspace/nvim-java-core'),
+				},
+				{
+					'nvim-java/nvim-java-test',
+					dir = f('~/Workspace/nvim-java-test'),
+				},
+				{
+					'nvim-java/nvim-java-dap',
+					dir = f('~/Workspace/nvim-java-dap'),
+				},
+				'MunifTanjim/nui.nvim',
+				'neovim/nvim-lspconfig',
+				'mfussenegger/nvim-dap',
+			},
+		},
+	},
+	opts = {
+		handlers = {
+			['jdtls'] = function()
+				require('lspconfig').jdtls.setup({})
+			end,
+		},
 	},
 }
--- return {
--- 	'nvim-java/nvim-java',
--- 	dependencies = {
--- 		{
--- 			'MunifTanjim/nui.nvim',
--- 			'nvim-neotest/neotest',
--- 			{
--- 				'nvim-java/lua-async-await',
--- 				-- dir = file.is_pkg_dir('~/Workspace/lua-async-await'),
--- 			},
--- 			dependencies = {
--- 				'nvim-lua/plenary.nvim',
--- 				'antoinemadec/FixCursorHold.nvim',
--- 				{
--- 					'nvim-java/neotest-jdtls',
--- 					-- dir = file.is_pkg_dir('~/Workspace/neotest-jdtls'),
--- 				},
--- 				'rcasia/neotest-java',
--- 			},
--- 		},
--- 		{
--- 			'nvim-java/nvim-java-test',
--- 			-- dir = file.is_pkg_dir('~/Workspace/nvim-java-test'),
--- 		},
--- 		{
--- 			'nvim-java/nvim-java-core',
--- 			-- dir = file.is_pkg_dir('~/Workspace/nvim-java-core'),
--- 		},
--- 		{
--- 			'nvim-java/nvim-java-dap',
--- 			-- dir = file.is_pkg_dir('~/Workspace/nvim-java-dap'),
--- 		},
--- 		'neovim/nvim-lspconfig',
--- 		{
--- 			'williamboman/mason.nvim',
--- 			opts = {
--- 				registries = {
--- 					'github:nvim-java/mason-registry',
--- 					'github:mason-org/mason-registry',
--- 				},
--- 			},
--- 		},
--- 		'mfussenegger/nvim-dap',
--- 	},
--- 	event = 'VeryLazy',
--- 	dir = file.is_pkg_dir('~/Workspace/nvim-java'),
--- 	opts = {
--- 		jdk = {
--- 			auto_install = false,
--- 		},
--- 	},
--- }
