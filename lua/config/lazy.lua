c3829335d0279c1f4ab7fe1f5477b6a86d55dca2
s1n7ax <srineshnisala@gmail.com>
2024-01-08 23:19:21 +0530

    feat: enable dap core lazy extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require('lazy').setup({
 		-- add LazyVim and import its plugins
 		{ 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
 		-- import any extras modules here
+		{ import = 'lazyvim.plugins.extras.dap.core' },
 		-- { import = "lazyvim.plugins.extras.lang.typescript" },
 		-- { import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
