0d53a93b49b25e3830e3c367434c6db0083f8fd3
s1n7ax <srineshnisala@gmail.com>
2024-01-09 08:13:18 +0530

    refactor: disable transparent popups

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,1 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.pumblend = 0
