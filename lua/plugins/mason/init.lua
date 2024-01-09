02a7605febe59e7d3efaf929c4500c1f465beb63
s1n7ax <srineshnisala@gmail.com>
2024-01-09 06:56:11 +0530

    chore: code clean up

--- lua/plugins/mason/init.lua
+++ lua/plugins/mason/init.lua
@@ -2,7 +2,6 @@ return {
 	'williamboman/mason.nvim',
 	opts = function(_, opts)
 		opts.ensure_installed = vim.tbl_filter(function(value)
-			vim.print(value ~= 'stylua' and value ~= 'shfmt')
 			return value ~= 'stylua' and value ~= 'shfmt'
 		end, opts.ensure_installed)
 
