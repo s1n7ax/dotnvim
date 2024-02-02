3880a1b1f4a1c3d85ea49728d8784ab74f1f1f08
s1n7ax <srineshnisala@gmail.com>
2024-02-02 18:52:48 +0530

    feat: disable ls installation via mason if executable exists

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -1,18 +1,34 @@
+local ls_to_exec_map = {
+	jsonls = 'vscode-json-language-server',
+	lua_ls = 'lua-language-server',
+	marksman = 'marksman',
+	nil_ls = 'nil',
+	rust_analyzer = 'rust-analyzer',
+	docker_compose_language_service = 'docker-compose-langserver',
+	dockerls = 'docker-langserver',
+	eslint = 'vscode-eslint-language-server',
+	pyright = nil,
+	ruff_lsp = 'ruff-lsp',
+	tailwindcss = 'tailwindcss-language-server',
+	taplo = 'taplo',
+	tsserver = 'tsserver',
+	yamlls = 'yaml-language-server',
+	hadolint = 'hadolint',
+}
+
 return {
 	{
 		'neovim/nvim-lspconfig',
-		opts = {
-			servers = {
-				lua_ls = {
-					mason = false,
-				},
-				marksman = {
-					mason = false,
-				},
-				nil_ls = {
-					mason = false,
-				},
-			},
-		},
+		opts = function(_, opts)
+			for server, _ in pairs(opts.servers) do
+				local exec = ls_to_exec_map[server]
+
+				if exec and vim.fn.executable(exec) then
+					opts.servers[server].mason = false
+				end
+			end
+
+			return opts
+		end,
 	},
 }
