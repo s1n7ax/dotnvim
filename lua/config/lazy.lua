aecf452614902cf076fe2d21d5742a5a7e2a3b1f
s1n7ax <srineshnisala@gmail.com>
2024-02-11 06:54:17 +0530

    refactor: limit to 5 plugin install processes at a time

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require('lazy').setup({
 		enabled = false,
 		notify = false, -- get a notification when changes are found
 	},
+	concurrency = 5,
 	spec = {
 		-- add LazyVim and import its plugins
 		{
