a99fd7e5d403d81f76a062ec6951b5dbe5c75571
s1n7ax <srineshnisala@gmail.com>
2024-02-11 06:58:21 +0530

    refactor: change keymaps for comment frame

--- lua/plugins/comment-frame/init.lua
+++ lua/plugins/comment-frame/init.lua
@@ -1,18 +1,23 @@
 return {
-  's1n7ax/nvim-comment-frame',
-  dependencies = {
-    { 'nvim-treesitter' }
-  },
-  keys = { '<leader><leader>cc', '<leader><leader>CC' },
-  opts = function()
-    local wk = require('which-key')
-
-    wk.register({
-      cc = { require('nvim-comment-frame').add_comment, 'Add comment frame' },
-      CC = { require('nvim-comment-frame').add_multiline_comment, 'Add multiline comment frame' }
-    }, {
-      prefix = '<leader><leader>'
-    })
-  end,
-  config = true
+	's1n7ax/nvim-comment-frame',
+	dependencies = {
+		{ 'nvim-treesitter' },
+	},
+	keys = {
+		{
+			'<leader>cc',
+			function()
+				require('nvim-comment-frame').add_comment()
+			end,
+			desc = 'Add comment frame',
+		},
+		{
+			'<leader><leader>CC',
+			function()
+				require('nvim-comment-frame').add_multiline_comment()
+			end,
+			'Add multiline comment frame',
+		},
+	},
+	config = true,
 }
