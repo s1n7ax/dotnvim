86b5eec33507eda0f485c28daa6e3d58bdab0466
s1n7ax <srineshnisala@gmail.com>
2024-02-23 10:49:56 +0530

    refactor: disable conform in favor of none-ls

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -1,11 +1,14 @@
 return {
 	'stevearc/conform.nvim',
+	enable = false,
 	opts = {
 		formatters_by_ft = {
 			fish = {},
 			lua = { 'stylua' },
 			python = { 'isort', 'black' },
 			javascript = { { 'prettierd' } },
+			json = { 'prettierd' },
+			yaml = { 'prettierd' },
 			markdown = { 'mdformat', 'cbfmt', 'markdownlint-cli2' },
 			nix = { 'nixfmt' },
 		},
---
+++ lua/plugins/none-ls/init.lua
@@ -0,0 +1,15 @@
+return {
+	'nvimtools/none-ls.nvim',
+	config = function()
+		local null_ls = require('null-ls')
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.completion.spell,
+				-- null_ls.builtins.formatting.prettierd,
+			},
+		})
+	end,
+}
