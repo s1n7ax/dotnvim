7a5081948660a8f71af78eb66ca6bdd66a9e7574
s1n7ax <srineshnisala@gmail.com>
2024-03-02 18:00:30 +0530

    feat: add javascript & typescript snips

--- lua/plugins/luasnip/snippets/javascript.lua
+++ lua/plugins/luasnip/snippets/javascript.lua
@@ -2,13 +2,31 @@ local M = {}
 
 function M.setup()
 	local ls = require('luasnip')
+	local common = require('snips.common')
 	local js = require('snips.javascript')
+	local ts = require('snips.typescript')
 
 	local s = ls.s
 	local i = ls.insert_node
 
-	local snippets = function()
+	local js_snippets = function()
 		return {
+			s('c', js.primitives.class()),
+			s('con', js.primitives.constructor()),
+			s('r', common.primitives.returns()),
+			s('des', js.primitives.describe()),
+			s('tc', js.primitives.test()),
+			s('exp', js.primitives.expect()),
+
+			s('fa', js.choices.anonymous_func()),
+
+			s('v', js.choices.variable()),
+			s('i', js.choices.stdout(), {
+				stored = {
+					value = i(1, 'value'),
+				},
+			}),
+
 			s('f', js.dynamic.func(), {
 				stored = {
 					name = i(1, 'name'),
@@ -16,20 +34,29 @@ function M.setup()
 					body = i(1),
 				},
 			}),
-			s('o', js.choices.stdout(), {
-				stored = {
-					value = i(1, 'value'),
-				},
-			}),
-			s('con', js.primitives.constructor()),
-			s('c', js.primitives.class()),
 		}
 	end
 
-	ls.add_snippets('javascript', snippets())
-	ls.add_snippets('typescript', snippets())
-	ls.add_snippets('javascriptreact', snippets())
-	ls.add_snippets('typescriptreact', snippets())
+	local ts_snippets = function()
+		return {
+			s('int', ts.primitives.interface()),
+			s('enum', ts.primitives.enum()),
+
+			s('v', ts.choices.variable()),
+		}
+	end
+
+	ls.add_snippets('javascript', js_snippets())
+	ls.add_snippets(
+		'typescript',
+		vim.tbl_extend('force', js_snippets(), ts_snippets())
+	)
+
+	ls.add_snippets('javascriptreact', js_snippets())
+	ls.add_snippets(
+		'typescriptreact',
+		vim.tbl_extend('force', js_snippets(), ts_snippets())
+	)
 end
 
 return M
