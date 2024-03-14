6fe71184e6b85fbcc08aa01967623bd62ed626d7
s1n7ax <srineshnisala@gmail.com>
2024-03-15 00:33:48 +0530

    refactor: remove color from the path when buffer is modified

--- lua/utils/nvim/winbar.lua
+++ lua/utils/nvim/winbar.lua
@@ -5,13 +5,7 @@ local existing_hi = vim.api.nvim_get_hl(0, { name = 'WinBar', link = false })
 vim.api.nvim_set_hl(
 	0,
 	'WinBarContentModified',
-	{ fg = '#b33434', bg = existing_hi.bg }
-)
-
-vim.api.nvim_set_hl(
-	0,
-	'WinBarPathModified',
-	{ fg = '#b33434', bg = existing_hi.bg }
+	{ fg = '#f54257', bg = existing_hi.bg }
 )
 
 M.eval = function()
@@ -19,15 +13,11 @@ M.eval = function()
 	local has_modified = vim.api.nvim_eval_statusline('%m', {}).str == '[+]'
 	local modified_status = has_modified and '  ' or ''
 
-	local winbar_path_hi = has_modified and 'WinBarPathModified' or 'WinBarPath'
-
 	file_path = file_path:gsub('/', ' ➤ ')
 
 	return '%#WinBarSeparator#'
 		.. '%*'
-		.. '%#'
-		.. winbar_path_hi
-		.. '#'
+		.. '%#WinBarPath#'
 		.. file_path
 		.. '%*'
 		.. '%#WinBarContentModified#'
