af236729f0d410c8dab225c4799cc6c982ac2dd3
s1n7ax <srineshnisala@gmail.com>
2024-03-14 21:55:50 +0530

    feat: enable winbar

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd('BufWinEnter', {
+	pattern = '*',
+	callback = function()
+		-- skip if a pop up window
+		if vim.fn.win_gettype() == 'popup' then
+			return
+		end
+
+		-- skip if new buffer
+		if vim.bo.filetype == '' then
+			return
+		end
+
+		vim.wo.winbar = "%{%v:lua.require'utils.nvim.winbar'.eval()%}"
+	end,
+	group = vim.api.nvim_create_augroup('WinBar', {}),
+})
--- lua/utils/nvim/winbar.lua
+++ lua/utils/nvim/winbar.lua
@@ -1,20 +1,37 @@
 local M = {}
 
+local existing_hi = vim.api.nvim_get_hl(0, { name = 'WinBar', link = false })
+
+vim.api.nvim_set_hl(
+	0,
+	'WinBarContentModified',
+	{ fg = '#b33434', bg = existing_hi.bg }
+)
+
+vim.api.nvim_set_hl(
+	0,
+	'WinBarPathModified',
+	{ fg = '#b33434', bg = existing_hi.bg }
+)
+
 M.eval = function()
 	local file_path = vim.api.nvim_eval_statusline('%f', {}).str
-	local modified = vim.api.nvim_eval_statusline('%m', {}).str == '[+]'
-			and '  '
-		or ''
+	local has_modified = vim.api.nvim_eval_statusline('%m', {}).str == '[+]'
+	local modified_status = has_modified and '  ' or ''
+
+	local winbar_path_hi = has_modified and 'WinBarPathModified' or 'WinBarPath'
 
 	file_path = file_path:gsub('/', ' ➤ ')
 
 	return '%#WinBarSeparator#'
 		.. '%*'
-		.. '%#WinBarContent#'
+		.. '%#'
+		.. winbar_path_hi
+		.. '#'
 		.. file_path
 		.. '%*'
 		.. '%#WinBarContentModified#'
-		.. modified
+		.. modified_status
 		.. '%*'
 		.. '%#WinBarSeparator#'
 		.. '%*'
