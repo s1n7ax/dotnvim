3da984185b12998f17e1dd5753056772d7d93cfc
s1n7ax <srineshnisala@gmail.com>
2024-03-14 23:52:32 +0530

    fix: eslint is not needed from none cz LS is available from lspconfig

--- lua/plugins/none-ls/init.lua
+++ lua/plugins/none-ls/init.lua
@@ -6,9 +6,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.completion.spell,
-				-- null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.prettierd,
+				-- null_ls.builtins.completion.spell,
 			},
 		})
 	end,
