b776d01b3e611d316aaf357bd6cc2f63066bf702
s1n7ax <srineshnisala@gmail.com>
2024-03-15 00:33:23 +0530

    fix: i will not move cursor to right in visual mode

---
+++ lua/plugins/mini-ai/init.lua
@@ -0,0 +1,4 @@
+return {
+	'echasnovski/mini.ai',
+	enabled = false,
+}
---
+++ lua/plugins/mini-indentscope/init.lua
@@ -0,0 +1,12 @@
+return {
+	'echasnovski/mini.indentscope',
+	opts = {
+		mappings = {
+			object_scope = '',
+			object_scope_with_border = '',
+
+			goto_top = '',
+			goto_bottom = '',
+		},
+	},
+}
