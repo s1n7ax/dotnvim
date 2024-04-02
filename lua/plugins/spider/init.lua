d8988bc78b33737ccc51ad30675cc7c89278fe68
s1n7ax <srineshnisala@gmail.com>
2024-04-02 20:03:11 +0530

    feat: add spider plugin

---
+++ lua/plugins/spider/init.lua
@@ -0,0 +1,25 @@
+local function motion(key, motion_type)
+	motion_type = motion_type or key
+
+	return {
+		key,
+		function()
+			require('spider').motion(motion_type)
+		end,
+		mode = { 'n', 'o', 'x' },
+	}
+end
+
+return {
+	'chrisgrieser/nvim-spider',
+	keys = {
+		motion('w'),
+		motion('l', 'e'),
+		motion('b'),
+		{ 'cw', 'ce' },
+		{ 'dw', 'de' },
+	},
+	opts = {
+		subwordMovement = false,
+	},
+}
