5bf860d678972af6e3c6ebc82b61ddffba28c65f
s1n7ax <srineshnisala@gmail.com>
2024-05-01 10:06:39 +0530

    fix: harpoon has default keymaps

--- lua/plugins/harpoon/init.lua
+++ lua/plugins/harpoon/init.lua
@@ -1,55 +1,57 @@
 return {
 	'ThePrimeagen/harpoon',
-	keys = {
-		{
-			',l',
-			function()
-				require('harpoon'):list():append()
-			end,
-			desc = 'Harpoon file',
-		},
-		{
-			',L',
-			function()
-				local harpoon = require('harpoon')
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end,
-			desc = 'Harpoon quick menu',
-		},
-		{
-			'<c-1>',
-			function()
-				require('harpoon'):list():select(1)
-			end,
-			desc = 'Harpoon to file 1',
-		},
-		{
-			'<c-2>',
-			function()
-				require('harpoon'):list():select(2)
-			end,
-			desc = 'Harpoon to file 2',
-		},
-		{
-			'<c-3>',
-			function()
-				require('harpoon'):list():select(3)
-			end,
-			desc = 'Harpoon to file 3',
-		},
-		{
-			'<c-4>',
-			function()
-				require('harpoon'):list():select(4)
-			end,
-			desc = 'Harpoon to file 4',
-		},
-		{
-			'<c-5>',
-			function()
-				require('harpoon'):list():select(5)
-			end,
-			desc = 'Harpoon to file 5',
-		},
-	},
+	keys = function()
+		return {
+			{
+				',l',
+				function()
+					require('harpoon'):list():append()
+				end,
+				desc = 'Harpoon file',
+			},
+			{
+				',L',
+				function()
+					local harpoon = require('harpoon')
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = 'Harpoon quick menu',
+			},
+			{
+				'<c-1>',
+				function()
+					require('harpoon'):list():select(1)
+				end,
+				desc = 'Harpoon to file 1',
+			},
+			{
+				'<c-2>',
+				function()
+					require('harpoon'):list():select(2)
+				end,
+				desc = 'Harpoon to file 2',
+			},
+			{
+				'<c-3>',
+				function()
+					require('harpoon'):list():select(3)
+				end,
+				desc = 'Harpoon to file 3',
+			},
+			{
+				'<c-4>',
+				function()
+					require('harpoon'):list():select(4)
+				end,
+				desc = 'Harpoon to file 4',
+			},
+			{
+				'<c-5>',
+				function()
+					require('harpoon'):list():select(5)
+				end,
+				desc = 'Harpoon to file 5',
+			},
+		}
+	end,
 }
