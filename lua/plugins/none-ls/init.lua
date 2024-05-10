7d6cad4ed4d057f70791b12c68bb3d7f7e92e448
s1n7ax <srineshnisala@gmail.com>
2024-05-10 07:24:37 +0530

    feat: enable nixfmt in none

--- lua/plugins/none-ls/init.lua
+++ lua/plugins/none-ls/init.lua
@@ -7,6 +7,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.nixfmt,
 				-- null_ls.builtins.completion.spell,
 			},
 		})
