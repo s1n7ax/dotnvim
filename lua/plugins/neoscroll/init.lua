46fd3ae5bb44d290801ebeba550e18d6829dd964
s1n7ax <srineshnisala@gmail.com>
2024-06-10 00:22:40 +0530

    refactor!: remove unwanted keymaps from neoscroll

--- lua/plugins/neoscroll/init.lua
+++ lua/plugins/neoscroll/init.lua
@@ -4,5 +4,16 @@ return {
 	opts = {
 		easing = 'quadratic',
 		hide_cursor = false,
+		mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+			'<C-u>',
+			'<C-d>',
+			-- '<C-b>',
+			-- '<C-f>',
+			-- '<C-y>',
+			-- '<C-e>',
+			'zt',
+			'zz',
+			'zb',
+		},
 	},
 }
