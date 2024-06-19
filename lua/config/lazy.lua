3f473bb9c769ff76acf9733342f9b08407e1d1c3
s1n7ax <srineshnisala@gmail.com>
2024-06-19 22:10:40 +0530

    chore: change color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ require('lazy').setup({
 		-- add LazyVim and import its plugins
 		{
 			'LazyVim/LazyVim',
-			opts = { colorscheme = 'catppuccin' },
+			opts = { colorscheme = 'tokyonight' },
 			import = 'lazyvim.plugins',
 		},
 		-- import any extras modules here
