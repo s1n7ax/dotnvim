b994a019fc01380f962835c11c1df6d05a8a9b47
s1n7ax <srineshnisala@gmail.com>
2024-06-24 20:21:56 +0530

    feat: add prettier for javascript and typescript files

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -2,5 +2,14 @@ return {
 	'stevearc/conform.nvim',
 	opts = function(_, opts)
 		opts.formatters_by_ft['nix'] = { 'nixfmt' }
+
+		for _, ft in ipairs({
+			'javascript',
+			'typescript',
+			'javascriptreact',
+			'typescriptreact',
+		}) do
+			opts.formatters_by_ft[ft] = { 'prettier' }
+		end
 	end,
 }
