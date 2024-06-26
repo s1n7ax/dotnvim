1ed918e690a4c80b2bddefbd04052ca0e075bab3
s1n7ax <srineshnisala@gmail.com>
2024-06-26 10:25:19 +0530

    feat: use eslint_d to lint

---
+++ lua/plugins/lint/init.lua
@@ -0,0 +1,11 @@
+return {
+	'mfussenegger/nvim-lint',
+	opts = {
+		linters_by_ft = {
+			javascript = { 'eslint_d' },
+			typescript = { 'eslint_d' },
+			javascriptreact = { 'eslint_d' },
+			typescriptreact = { 'eslint_d' },
+		},
+	},
+}
