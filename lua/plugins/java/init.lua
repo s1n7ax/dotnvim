fb91f10af6fac39355446645d9df5ee103bdcde2
s1n7ax <srineshnisala@gmail.com>
2024-06-29 23:23:06 +0530

    fix: order of exec of nvim-java is not correct

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,8 +1,33 @@
 local f = require('utils.file').is_pkg_dir
 
 return {
-	'williamboman/mason-lspconfig.nvim',
+	'nvim-java/nvim-java',
+	dir = f('~/Workspace/nvim-java'),
+	config = false,
 	dependencies = {
+		{
+			'nvim-java/lua-async-await',
+			dir = f('~/Workspace/lua-async-await'),
+		},
+		{
+			'nvim-java/nvim-java-core',
+			dir = f('~/Workspace/nvim-java-core'),
+		},
+		{
+			'nvim-java/nvim-java-test',
+			dir = f('~/Workspace/nvim-java-test'),
+		},
+		{
+			'nvim-java/nvim-java-dap',
+			dir = f('~/Workspace/nvim-java-dap'),
+		},
+		{
+			'nvim-java/nvim-java-refactor',
+			dir = f('~/Workspace/nvim-java-refactor'),
+		},
+		'MunifTanjim/nui.nvim',
+		'neovim/nvim-lspconfig',
+		'mfussenegger/nvim-dap',
 		{
 			'williamboman/mason.nvim',
 			opts = {
@@ -12,50 +37,22 @@ return {
 				},
 			},
 		},
-
 		{
-			'nvim-java/nvim-java',
-			dir = f('~/Workspace/nvim-java'),
+			'williamboman/mason-lspconfig.nvim',
 			opts = {
-				jdk = {
-					auto_install = false,
-				},
-				notifications = {
-					dap = false,
+				handlers = {
+					['jdtls'] = function()
+						require('java').setup({
+							jdk = {
+								auto_install = false,
+							},
+							notifications = {
+								dap = false,
+							},
+						})
+					end,
 				},
 			},
-			dependencies = {
-				{
-					'nvim-java/lua-async-await',
-					dir = f('~/Workspace/lua-async-await'),
-				},
-				{
-					'nvim-java/nvim-java-core',
-					dir = f('~/Workspace/nvim-java-core'),
-				},
-				{
-					'nvim-java/nvim-java-test',
-					dir = f('~/Workspace/nvim-java-test'),
-				},
-				{
-					'nvim-java/nvim-java-dap',
-					dir = f('~/Workspace/nvim-java-dap'),
-				},
-				{
-					'nvim-java/nvim-java-refactor',
-					dir = f('~/Workspace/nvim-java-refactor'),
-				},
-				'MunifTanjim/nui.nvim',
-				'neovim/nvim-lspconfig',
-				'mfussenegger/nvim-dap',
-			},
-		},
-	},
-	opts = {
-		handlers = {
-			['jdtls'] = function()
-				require('lspconfig').jdtls.setup({})
-			end,
 		},
 	},
 }
