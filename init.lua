70e4bb7ccceb41ee59cdd4a7119f81a338775ada
s1n7ax <srineshnisala@gmail.com>
2024-07-24 22:50:05 +0530

    fix: accidentally committed temp keymaps

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require('config.lazy')
-
-vim.keymap.set('n', '<leader><leader>s', '<cmd>JavaTestRunCurrentClass<cr>')
-vim.keymap.set('n', '<leader><leader>r', '<cmd>JavaTestViewLastReport<cr>')
