295123af60f4e4d13e8d31c4a3b8c41c9fe0b501
s1n7ax <srineshnisala@gmail.com>
2024-07-25 12:25:46 +0530

    feat: enable lazy inner block

--- lua/plugins/lazy-inner-block/init.lua
+++ lua/plugins/lazy-inner-block/init.lua
@@ -2,7 +2,6 @@ local f = require('utils.file').is_pkg_dir
 
 return {
 	's1n7ax/nvim-lazy-inner-block',
-	enabled = false,
 	dir = f('~/Workspace/nvim-lazy-inner-block'),
 	event = 'VeryLazy',
 	config = true,
