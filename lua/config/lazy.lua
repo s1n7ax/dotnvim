cee5846acdf4673c6f5a38d4ebce4ec6cbacfb67
s1n7ax <srineshnisala@gmail.com>
2024-07-25 23:47:37 +0530

    chore: add core extras to lazy config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,12 +25,10 @@ require('lazy').setup({
 			opts = { colorscheme = 'tokyonight-moon' },
 			import = 'lazyvim.plugins',
 		},
-		-- import any extras modules here
-		-- { import = 'lazyvim.plugins.extras.dap.core' },
-		-- { import = 'lazyvim.plugins.extras.lang.typescript' },
-		-- { import = 'lazyvim.plugins.extras.lang.json' },
-		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-		-- import/override with your plugins
+		{ import = 'lazyvim.plugins.extras.test.core' },
+		{ import = 'lazyvim.plugins.extras.dap.core' },
+		{ import = 'lazyvim.plugins.extras.lang.json' },
+
 		{ import = 'plugins' },
 	},
 	defaults = {
