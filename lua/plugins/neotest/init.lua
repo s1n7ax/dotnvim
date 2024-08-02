42a566519470b86f0e1782c5f2b62bb2d330869a
s1n7ax <srineshnisala@gmail.com>
2024-08-03 01:24:52 +0530

    feat: add neotest-jdtls plugin

--- lua/plugins/neotest/init.lua
+++ lua/plugins/neotest/init.lua
@@ -1,7 +1,15 @@
+local f = require('utils.file').is_pkg_dir
+
 return {
 	'nvim-neotest/neotest',
 	optional = true,
-	dependencies = { 'nvim-neotest/neotest-jest' },
+	dependencies = {
+		'nvim-neotest/neotest-jest',
+		{
+			'nvim-java/neotest-jdtls',
+			dir = f('~/Workspace/neotest-jdtls'),
+		},
+	},
 	opts = function()
 		return {
 			adapters = {
@@ -12,6 +20,9 @@ return {
 						return vim.fn.getcwd()
 					end,
 				}),
+				['neotest-jdtls'] = function()
+					return require('neotest-jdtls')
+				end,
 			},
 		}
 	end,
