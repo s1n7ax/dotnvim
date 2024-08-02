773abfed5ffc81f2953cfda02eb677d86675d7f8
s1n7ax <srineshnisala@gmail.com>
2024-08-03 01:17:43 +0530

    fix: adding terminal border fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,21 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
 	end,
 	group = vim.api.nvim_create_augroup('WinBar', {}),
 })
+
+---Following fixes the border issue in terminals
+---@see https://www.reddit.com/r/neovim/comments/1ehidxy/you_can_remove_padding_around_neovim_instance/
+vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+		if not normal.bg then
+			return
+		end
+		io.write(string.format('\027]11;#%06x\027\\', normal.bg))
+	end,
+})
+
+vim.api.nvim_create_autocmd('UILeave', {
+	callback = function()
+		io.write('\027]111\027\\')
+	end,
+})
