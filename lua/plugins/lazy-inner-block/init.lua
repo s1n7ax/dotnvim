695aef3e8c3e0297a0b09e2610725b0c4861891f
s1n7ax <srineshnisala@gmail.com>
2024-08-04 09:07:47 +0530

    chore: use lazy-inner-block local files if exists

--- lua/plugins/lazy-inner-block/init.lua
+++ lua/plugins/lazy-inner-block/init.lua
@@ -2,7 +2,7 @@ local f = require('utils.file').is_pkg_dir
 
 return {
 	's1n7ax/nvim-lazy-inner-block',
-	dir = f('~/Workspace/nvim-lazy-inner-block'),
+	dir = f('~/Workspace/my-projects/nvim-lazy-inner-block'),
 	event = 'VeryLazy',
 	config = true,
 }
