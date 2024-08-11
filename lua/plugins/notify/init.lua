61c62cb8abe5ec5772c705f111b3cfac6a87bba6
s1n7ax <srineshnisala@gmail.com>
2024-08-12 01:16:56 +0530

    chore(ui): make notify more compact

--- lua/plugins/notify/init.lua
+++ lua/plugins/notify/init.lua
@@ -1,7 +1,9 @@
 return {
 	'rcarriga/nvim-notify',
-	event = 'VeryLazy',
+	optional = true,
 	opts = {
-		background_colour = '#111111',
+		minimum_width = 5,
+		max_width = 30,
+		render = 'wrapped-compact',
 	},
 }
