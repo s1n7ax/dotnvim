6b3ef4b8c741a27fe1ae3bb50ea3706060940874
s1n7ax <srineshnisala@gmail.com>
2024-08-14 20:15:57 +0530

    feat: add markdown extra

--- lua/plugins/markview/init.lua
+++ lua/plugins/markview/init.lua
@@ -1,8 +1,21 @@
 return {
-	'OXY2DEV/markview.nvim',
-	ft = 'markdown',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter',
-		'nvim-tree/nvim-web-devicons',
+	{
+		'OXY2DEV/markview.nvim',
+		ft = 'markdown',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter',
+			'nvim-tree/nvim-web-devicons',
+		},
+	},
+	-- Following dependencies are comming from lazyvim markdown extra
+	{
+		'iamcco/markdown-preview.nvim',
+		optional = true,
+		enabled = false,
+	},
+	{
+		'MeanderingProgrammer/markdown.nvim',
+		optional = true,
+		enabled = false,
 	},
 }
