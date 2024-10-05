f1ea9d38ef987c73d896c87bd797af726f67ee92
s1n7ax <srineshnisala@gmail.com>
2024-10-05 10:38:51 +0530

    feat: add llm-ls

---
+++ lua/plugins/llm/init.lua
@@ -0,0 +1,29 @@
+return {
+	'huggingface/llm.nvim',
+	event = 'VeryLazy',
+	lazy = false,
+	opts = {
+		lsp = {
+			bin_path = '/etc/profiles/per-user/s1n7ax/bin/llm-ls',
+			cmd_env = { LLM_LOG_LEVEL = 'DEBUG' },
+		},
+
+		backend = 'ollama',
+		model = 'deepseek-coder:6.7b-base',
+		url = 'http://localhost:11434', -- llm-ls uses "/api/generate"
+		-- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+		fim = {
+			enabled = true,
+			prefix = '<｜fim▁begin｜>',
+			suffix = '<｜fim▁hole｜>',
+			middle = '<｜fim▁end｜>',
+		},
+		request_body = {
+			-- Modelfile options for the model you use
+			options = {
+				temperature = 0.2,
+				top_p = 0.95,
+			},
+		},
+	},
+}
