d5909f75a489a55ce65544f79cfa8c456e79bfbb
s1n7ax <srineshnisala@gmail.com>
2024-10-17 22:17:42 +0530

    feat: add eslint using lsp instead of nvim-lint

--- lua/plugins/lint/init.lua
+++ lua/plugins/lint/init.lua
@@ -3,10 +3,6 @@ return {
 	event = 'VeryLazy',
 	opts = {
 		linters_by_ft = {
-			javascript = { 'eslint_d' },
-			typescript = { 'eslint_d' },
-			javascriptreact = { 'eslint_d' },
-			typescriptreact = { 'eslint_d' },
 			lua = { 'luacheck' },
 		},
 	},
