31edf521e0738c91a11c5cd0a841dbc09a73e2cd
s1n7ax <srineshnisala@gmail.com>
2024-11-23 00:13:59 +0530

    refactor!: switch to mini.surround

--- lua/plugins/surround/init.lua
+++ lua/plugins/surround/init.lua
@@ -1,5 +1,15 @@
 return {
-	'kylechui/nvim-surround',
-	event = 'VeryLazy',
-	opts = {},
+	'echasnovski/mini.surround',
+
+	opts = {
+		mappings = {
+			add = ',sa', -- Add surrounding in Normal and Visual modes
+			delete = ',sd', -- Delete surrounding
+			find = ',sf', -- Find surrounding (to the right)
+			find_left = ',sF', -- Find surrounding (to the left)
+			highlight = ',sh', -- Highlight surrounding
+			replace = ',sr', -- Replace surrounding
+			update_n_lines = ',sn', -- Update `n_lines`
+		},
+	},
 }
