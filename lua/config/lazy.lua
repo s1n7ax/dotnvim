229b7eb60760b0c2401b7dab40689e9f5fdd6245
s1n7ax <srineshnisala@gmail.com>
2025-04-13 03:35:18 +0530

    feat: enable luasnip extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,7 @@ require('lazy').setup({
 		},
 		{ import = 'lazyvim.plugins.extras.ai.copilot' },
 		{ import = 'lazyvim.plugins.extras.ai.copilot-chat' },
-		{ import = 'lazyvim.plugins.extras.coding.mini-surround' },
+		{ import = 'lazyvim.plugins.extras.coding.luasnip' },
 		{ import = 'lazyvim.plugins.extras.dap.core' },
 		{ import = 'lazyvim.plugins.extras.editor.harpoon2' },
 		{ import = 'lazyvim.plugins.extras.editor.overseer' },
