f52c3d8511857f70139cfa5539e53a52ccf840c4
s1n7ax <srineshnisala@gmail.com>
2025-04-12 15:37:56 +0530

    feat: add prettier extra

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -1,31 +1,31 @@
 return {
 	'stevearc/conform.nvim',
 	optional = true,
-	opts = function(_, opts)
-		opts.formatters_by_ft['nix'] = { 'nixfmt' }
-
-		for _, ft in ipairs({
-			'css',
-			'graphql',
-			'handlebars',
-			'html',
-			'javascript',
-			'javascriptreact',
-			'json',
-			'jsonc',
-			'less',
-			'markdown',
-			'markdown.mdx',
-			'scss',
-			'typescript',
-			'typescriptreact',
-			'vue',
-			'yaml',
-			'svelte',
-		}) do
-			opts.formatters_by_ft[ft] = { 'prettierd' }
-		end
-
-		opts.formatters_by_ft['sql'] = { 'pg_format' }
-	end,
+	-- opts = function(_, opts)
+	-- 	opts.formatters_by_ft['nix'] = { 'nixfmt' }
+	--
+	-- 	for _, ft in ipairs({
+	-- 		'css',
+	-- 		'graphql',
+	-- 		'handlebars',
+	-- 		'html',
+	-- 		'javascript',
+	-- 		'javascriptreact',
+	-- 		'json',
+	-- 		'jsonc',
+	-- 		'less',
+	-- 		'markdown',
+	-- 		'markdown.mdx',
+	-- 		'scss',
+	-- 		'typescript',
+	-- 		'typescriptreact',
+	-- 		'vue',
+	-- 		'yaml',
+	-- 		'svelte',
+	-- 	}) do
+	-- 		opts.formatters_by_ft[ft] = { 'prettierd' }
+	-- 	end
+	--
+	-- 	opts.formatters_by_ft['sql'] = { 'pg_format' }
+	-- end,
 }
