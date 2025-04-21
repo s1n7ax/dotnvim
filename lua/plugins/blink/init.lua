53b66ae5d45ac1de87363bbf59da46cb16cef59f
s1n7ax <srineshnisala@gmail.com>
2025-04-22 00:09:31 +0530

    chore: disable ghost text

--- lua/plugins/blink/init.lua
+++ lua/plugins/blink/init.lua
@@ -2,14 +2,16 @@ return {
 	'saghen/blink.cmp',
 	optional = true,
 	enabled = true,
-	-- dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+	dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+	---@type blink.cmp.Config
 	opts = {
 		snippets = { preset = 'luasnip' },
-		signature = { enabled = true },
+		signature = { enabled = true, window = { border = 'single' } },
 		sources = {
 			default = { 'lsp', 'path', 'buffer' },
 
 			transform_items = function(_, items)
+				-- removes snippes from completion items
 				return vim.tbl_filter(function(item)
 					return item.kind
 						~= require('blink.cmp.types').CompletionItemKind.Snippet
@@ -23,6 +25,13 @@ return {
 			['<c-n>'] = { 'select_next', 'snippet_forward', 'fallback' },
 			['<c-e>'] = { 'select_prev', 'snippet_backward', 'fallback' },
 			['<cr>'] = { 'select_and_accept', 'fallback' },
+		},
+		completion = {
+			menu = { border = 'single' },
+			documentation = { window = { border = 'single' } },
+			ghost_text = {
+				enabled = false,
+			},
 		},
 	},
 }
